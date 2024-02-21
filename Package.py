9b6ac2e3e3710a98d86693bd9b3eb68e6513d56e
Gavin Taylor <gavintaylor.ai@gmail.com>
2024-02-21 12:28:30 -0600

    Updated truck and package files. Added student ID on the top of Main file.

--- Package.py
+++ Package.py
@@ -1,13 +1,31 @@
 class Package:
-    def __init__(self, packageID, packageDestination, packageStatus):
+    def __init__(self, packageID, packageDeliveryAddress, packageDeliveryCity, packageDeliveryState, packageDeliveryZip, packageDeliveryDeadline, packageWeight, packageStatus):
     # object properties
         self.packageID = packageID
-        self.packageDestination = packageDestination
+        self.packageDeliveryAddress = packageDeliveryAddress
+        self.packageDeliveryCity = packageDeliveryCity
+        self.packageDeliveryState = packageDeliveryState
+        self.packageDeliveryZip = packageDeliveryZip
+        self.packageDeliveryDeadline = packageDeliveryDeadline
+        self.packageWeight = packageWeight
         self.packageStatus = packageStatus
+        self.packageDepartureTime = None
+        self.packageDeliveryTime = None
 
     def __str__(self):
     # what is returned if object is string
+        return "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s" % (
+            self.packageID,
+            self.packageDeliveryAddress,
+            self.packageDeliveryCity,
+            self.packageDeliveryState,
+            self.packageDeliveryZip,
+            self.packageDeliveryDeadline,
+            self.packageWeight,
+            self.packageStatus,
+            self.packageDepartureTime,
+            self.packageDeliveryTime
+        )
 
-
-    def getPackageStatus(self):
+    # def getPackageStatus(self):
     # determine whether the package is at the hub, en route, or delivered
--- Truck.py
+++ Truck.py
@@ -1,9 +1,22 @@
 class Truck:
-    def __init__(self, speed, maxLoad):
+    def __init__(self, maxLoad, truckSpeed, loadPackages, packages, distanceTraveled, deliveryAddress, timeDeparted):
         # object properties
-        self.speed = 18
         self.maxLoad = 16
+        self.truckSpeed = 18
+        self.loadPackages = loadPackages
+        self.packages = packages
+        self.distanceTraveled = distanceTraveled
+        self.deliveryAddress = deliveryAddress
+        self.timeDeparted = timeDeparted
 
     def __str__(self):
         # what should be returned when object is represented as string
-
+        return "%s, %s, %s, %s, %s, %s, %s" % (
+            self.maxLoad,
+            self.truckSpeed,
+            self.loadPackages,
+            self.packages,
+            self.distanceTraveled,
+            self.deliveryAddress,
+            self.timeDeparted
+        )
